c6b98e660f4e198f9b38c51e4ece92ab8a1640f1
Vishal Dubey <79298248+shinch4n@users.noreply.github.com>
2022-12-07 11:34:53 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,8 @@ go install github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/gf@latest
+go install github.com/KathanP19/Gxss@latest
+go install github.com/hahwul/dalfox@latest
 mkdir ~/.gf
 git clone https://github.com/1ndianl33t/Gf-Patterns.git
 cd Gf-Patterns
